fd95a3a127b6988561eb2f32fbe25cc8bb18c867
AhmedEhabH <ahmed.ehab.h@gmail.com>
2021-04-24 00:26:16 +0200

    get all products done get product by id done

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db")();
 
+
 const app = express();
+app.use(express.json());
+const routes = require("./routes/index")(app);
+
 
 const PORT = process.env.PORT || 5000;
 
