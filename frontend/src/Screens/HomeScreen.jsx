cbd86476c47d8f695ae54260d810d96c2e839518
AhmedEhabH <ahmed.ehab.h@gmail.com>
2021-04-26 03:30:48 +0200

    start working in home screen add product add some style make it responsive

--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -1,5 +1,21 @@
 import "./HomeScreen.css";
 
+import Product from '../components/Product';
+
 export default function HomeScreen() {
-    return <div className="homescreen">Home Screen</div>;
+    return <div className="homescreen">
+        <h2 className="homescreen_title">Latest Products</h2>
+        <div className="homescreen__products">
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+            <Product />
+        </div>
+    </div>;
 }
